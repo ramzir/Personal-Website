2df96cd48ef2e40a6744592b427284579b1fbc03
Hana Rimawi <rrimaw2@uic.edu>
2021-08-28 12:22:46 -0500

    Update scrolling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-// import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import crawfish from '../assets/images/crawfish.jpg'
 import NavBar from './Navbar'
 import Section from './Section'
 import Resume from './Resume'
 import down from '../assets/icons/chevron-down.svg'
 import Contact from './Contact';
-import smooth from './smooth.js';
-
 
 class Home extends Component {
-    render() {
-      if(this.props.data){
+   componentDidMount() {
+      var downImg = document.getElementById('downImg');
+      downImg.addEventListener('click', () => window.scrollTo({
+          top: document.getElementById("about").offsetTop - 20,
+          behavior: 'smooth',
+      }));
+   }
 
-      }
-  
+    render() {
       return (
          <div class="fullheight">
          <head>
@@ -31,14 +32,13 @@ class Home extends Component {
                <p class="fade-in-bottom">
                I'm a student at the University of Illinois in Chicago studying Computer Science to become a Software Engineer
                </p>
-               <img href="about" class="slide-top" src={down}></img>
+               <img id='downImg' href="about" class="slide-top" src={down}></img>
                </div>
             </div>
         </section>
         <Section></Section>
         <Resume></Resume>
         <Contact></Contact>
-        <script src={smooth} type="text/javascript"></script>    
         </div>
       );
     }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { Component } from 'react';
 import logo from '../assets/icons/rr.png'
 
-export default () => (
-    <div>
-        <nav class="navbar navbar-top">
-            <a class="navbar-brand fade-in" href="/">
-                <img src={logo} height="50px" class="d-inline-block align-top" loading="lazy"/>
-            </a>
-            <div class="navbartext fade-in">
-                <a class="navbar-text" id="homeNav">
-                    HOME
+class Navbar extends Component {
+    componentDidMount() {
+        var homeNav = document.getElementById('homeNav');
+        homeNav.addEventListener('click', () => window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        }));
+
+        var aboutNav = document.getElementById('aboutNav');
+        aboutNav.addEventListener('click', () => window.scrollTo({
+            top: document.getElementById("about").offsetTop - 20,
+            behavior: 'smooth',
+        }))
+
+        document.getElementById("resumeNav").addEventListener('click', () => window.scrollTo({
+            top: document.getElementById("resume").offsetTop,
+            behavior: 'smooth',
+        }));
+    
+        document.getElementById("contactNav").addEventListener('click', () => window.scrollTo({
+            top: document.getElementById("contact").offsetTop,
+            behavior: 'smooth',
+        }));
+    }
+  
+    render () {
+        return (
+            <div>
+            <nav class="navbar navbar-top">
+                <a class="navbar-brand fade-in" href="/">
+                    <img src={logo} height="50px" class="d-inline-block align-top" loading="lazy"/>
                 </a>
-                <a class="navbar-text" id="aboutNav">
-                    ABOUT
-                </a>
-                <a class="navbar-text" id="resumeNav">
-                    RESUME
-                </a>
-                <a class="navbar-text" id="contactNav">
-                    CONTACT
-                </a>
-            </div>
-        </nav>
-    </div>
-    );
+                <div class="navbartext fade-in">
+                    <a class="navbar-text" id="homeNav">
+                        HOME
+                    </a>
+                    <a class="navbar-text" id="aboutNav">
+                        ABOUT
+                    </a>
+                    <a class="navbar-text" id="resumeNav">
+                        RESUME
+                    </a>
+                    <a class="navbar-text" id="contactNav">
+                        CONTACT
+                    </a>
+                </div>
+            </nav>
+        </div>
+        )
+    }
+}
+export default Navbar;
